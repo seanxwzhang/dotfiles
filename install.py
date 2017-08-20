e848818677409c1062d72e05ad030cbe497e6c3a
Xiaowen Zhang <xiaowen@uber.com>
2017-08-19 23:37:46 -0700

    added install script

---
+++ install.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# This script installs my dotfiles to the ~ directory
+import shutil
+import os
+import errno
+
+ITEMS = [
+    ".vimrc",
+    ".vim",
+    ".vim_runtime",
+    ".zsh",
+    ".zshrc",
+    ".oh-my-zsh",
+    ".zshrc.pre-oh-my-zsh",
+    ".tmux",
+    ".tmux.conf.local"
+]
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def install():
+    scriptPath = os.path.realpath(__file__)
+    dirPath = os.path.dirname(scriptPath)
+    homePath = os.path.expanduser('~')
+    archivePath = os.path.join(dirPath, 'archive')
+    shutil.rmtree(archivePath)
+    mkdir_p(os.path.join(archivePath))
+    for item in ITEMS:
+        itemDest = os.path.join(homePath, item)
+        itemSrc = os.path.join(dirPath, item)
+        print("installing %s" % item)
+        if os.path.exists(itemDest):
+            shutil.move(itemDest, archivePath)
+        os.symlink(itemSrc, itemDest)
+    return
+
+if __name__ == "__main__":
+    install()
+
