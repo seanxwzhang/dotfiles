5fb6b35543b642741e35dff78933f76883dd9177
Xiaowen Zhang <seanxwzhang@gmail.com>
2017-08-20 12:33:44 -0700

    fix ff

--- .oh-my-zsh/tools/ff.sh
+++ .oh-my-zsh/tools/ff.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # Recursively find file/folders and display them
-find $PWD -iname "*$1*" -maxdepth 5 | grep -i --color=always $1 | sed -E "s#$PWD#\.#g"
+find $PWD -maxdepth 5 -iname "*$1*" | grep -i --color=always $1 | sed -E "s#$PWD#\.#g"
